9c01926618e608af572f2a064ef914a3b0866f19
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-10 11:23:11 +0200

    update

--- UnderTheMoonProject/src/App.jsx
+++ UnderTheMoonProject/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { ShowGallery } from './components/gallery'
 import './components/gallery.css'
+import HeaderBg from './assets/img/HeaderBackground.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,13 +28,19 @@ function App() {
   <a href="#about">OM</a>
   <a href="#contact">KONTAKT</a>
 </div>
-      <h1>Under the Moon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-      </div>
+      <h1 className='header'>Under the Moon</h1>
+      <img className='Bg' src={HeaderBg}/>
+      <div class="text-block">
+    <h4>HAIRSTYLIST OG FRISØR</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis, 
+      lacus sed volutpat molestie, orci arcu pretium elit, non cursus nunc eros at est. 
+      Vivamus sed tortor et tellus pretium mollis malesuada rhoncus magna. 
+      Duis ullamcorper feugiat lacus, interdum malesuada nibh aliquam et. 
+      Aenean hendrerit porttitor bibendum. Nunc commodo nulla eget urna molestie, 
+      viverra tincidunt justo tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+      per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+      Vestibulum ut efficitur nulla. Maecenas et lobortis nunc.</p>
+  </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
