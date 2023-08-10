3a5d7d0b3cea4678ca17eb30c94bf638e3ad03de
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-10 10:00:23 +0200

    update

--- UnderTheMoonProject/src/App.jsx
+++ UnderTheMoonProject/src/App.jsx
@@ -8,22 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+<div class="topnav">
+  <a class="active" href="#home">HJEM</a>
+  <a href="#news">KONCEPTER</a>
+  <a href="#contact">UTM LOYALTY</a>
+  <a href="#about">OM</a>
+  <a href="#contact">KONTAKT</a>
+</div>
+      <h1>Under the Moon</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
